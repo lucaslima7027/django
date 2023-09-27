6d144d2eb21eb69a2a8ee311dfc2c48e2126ac75
Lucas Lima <limalucas7027@gmail.com>
2023-09-27 20:39:55 -0300

    Testing django form

--- streaming_vod/views.py
+++ streaming_vod/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django import forms
 from django.shortcuts import render
+
+class newTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -8,11 +12,23 @@ def index(request):
 def greet(request, name):
     return render(request, "streaming_vod/greet.html", {"name":name.capitalize()})
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 def index1(request):
     return render(request, "streaming_vod/tasks.html", {
         "tasks": tasks
         })
 
 def add(request):
-    return render(request, "streaming_vod/add.html")
+    if request.method == "POST":
+        form = newTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "streaming_vod/add.htm", {
+                "form": form
+            })
+
+    return render(request, "streaming_vod/add.html", {
+        "form": newTaskForm()
+    })
